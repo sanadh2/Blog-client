45e75bc511fe8bef45c291436cd336472c7d0a43
sanadh2 <suriyaakku@gmail.com>
2024-01-05 21:18:58 +0530

    first commit

--- src/Contexts/UserData.jsx
+++ src/Contexts/UserData.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 
 export const UserData = createContext();
@@ -5,6 +6,17 @@ export const UserData = createContext();
 const UserDataProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
   const [refreshUser, setRefreshUser] = useState(false);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await axios.get("https://api.example.com/user");
+
+      setUserData(data);
+    } catch (error) {
+      console.error("Error fetching user data", error);
+    }
+  };
+
   return (
     <UserData.Provider
       value={{ userData, setUserData, refreshUser, setRefreshUser }}
